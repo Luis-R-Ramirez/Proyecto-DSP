deddde2c8e1294b64228e430b953efd8374a639d
Luis-R-Ramirez <12229sz@gmail.com>
2014-05-12 23:02:50 -0430

    Agregados comentarios a las variables

--- test.py
+++ test.py
@@ -5,12 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-N = 1000.
-t = np.r_[0.:N]/1250
+N = 1000.  # length
+F = 1250.   # sample frequency
+T = 1./F    # sample time
+t = np.r_[0.:N]*T
 F = np.zeros((N,20))
 for K in range(0,20):
-    F[:,K] = np.sin(2*np.pi*t*K*440)
+    F[:,K] = np.sin(2*np.pi*K*440*t)
 
-plt.plot(t, F[:,6])
+plt.plot(t, F[:,2])
 plt.show()
 
